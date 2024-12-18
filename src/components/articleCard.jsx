ae94f773d13c17126fef7befb5f79b7366d0e9c6
yahya436 <yahyaislam436@gmail.com>
2024-12-18 12:32:16 +0000

    finish task 5

---
+++ src/components/articleCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+const ArticleCard = ({ article }) => {
+  return (
+    <div className="article-card">
+      <img
+        src={article.article_img_url}
+        alt={article.title}
+        className="article-image"
+      />
+      <h2 className="article-title">{article.title}</h2>
+      <p className="article-author">By {article.author}</p>
+      <p className="article-topic">Topic: {article.topic}</p>
+      <p className="article-votes">Votes: {article.votes}</p>
+      <Link to={`/articles/${article.article_id}`} className="read-more">
+      Read more
+      </Link>
+    </div>
+  );
+};
+
+export default ArticleCard;
